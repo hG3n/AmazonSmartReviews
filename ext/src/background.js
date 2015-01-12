6c51d1c369bce7b626967c2eef489590065c8aea
Hagen Hiller <h.hiller@me.com>
2015-01-12 18:54:43 +0100

    fixed path problem

--- ext/src/background.js
+++ ext/src/background.js
@@ -5,8 +5,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   amazon = url.substring(11,17);
   product = url.substring(24,31);
 
-  if(amazon == "amazon" /*&& product == "product"*/)
-    chrome.tabs.executeScript( {file: "src/reviews.js" });
+  if(amazon == "amazon")
+    chrome.tabs.executeScript( {file: "reviews.js" });
   else
     alert("This is just for Amazon");
 
