7fe009d716d68015b1ba3e87e09a38be174186bb
Hagen Hiller <h.hiller@me.com>
2015-01-12 18:45:08 +0100

    fixed error concerning different URL's on different OS

--- ext/src/background.js
+++ ext/src/background.js
@@ -5,7 +5,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   amazon = url.substring(11,17);
   product = url.substring(24,31);
 
-  if(amazon == "amazon" && product == "product")
+  if(amazon == "amazon" /*&& product == "product"*/)
     chrome.tabs.executeScript( {file: "src/reviews.js" });
   else
     alert("This is just for Amazon");
