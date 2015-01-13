092653367c9dcb7a00c0e236188f94211ec82ee2
Hagen Hiller <h.hiller@me.com>
2015-01-13 12:55:47 +0100

    added setup functions, need to fix bug that d3 ist not recognized

---
+++ ext/background.js
@@ -0,0 +1,12 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  
+  var url = tab.url;
+  var amazon = url.substring(11,17);
+  var product = url.substring(24,31);
+
+  if(amazon == "amazon")
+    chrome.tabs.executeScript( {file: "reviews.js" });
+  else
+    alert("This is just for Amazon-Product Sites");
+
+});
--- ext/reviews.js
+++ ext/reviews.js
@@ -1,1 +1,53 @@
-console.log("foo");
+// ---------------------------- //
+// --- AMAZON SMART REVIEWS --- //
+// ---------------------------- //
+// ----- Authors: ------------- //
+// ----- Hagen Hiller --------- //
+// ----- Ephraim Schott ------- //
+// ---------------------------- //
+
+// global variables 
+var w = window.innerWidth - 50;
+var h = 50;
+
+var setupDependencies = function() {
+  var head = document.getElementsByTagName("head");
+  script = document.createElement("script");
+  script.src = "http://d3js.org/d3.v3.min.js";
+  script.charset = "utf-8";
+  head[0].appendChild(script);
+}
+
+var setupStructure = function() {
+  // clear the review div
+  var reviewDiv = document.getElementById("cm_cr_dpwidget");
+  reviewDiv.innerHTML = "";
+
+  // create div for header and further informations
+  var header = document.createElement("div");
+  header.className = "smartHeader";
+  reviewDiv.appendChild(header);
+
+  // create div for review visualisation
+  var reviewBars = document.createElement("div");
+  reviewBars.className = "smartReviews";
+  reviewDiv.appendChild(reviewBars);
+}
+
+var initSmartReviews = function() {
+  
+  // TODO: fix d3 include - currently not working
+  var smartReviewSVG = d3.select(".smartReviews")
+                         .append("svg")
+                         .attr("width", w)
+                         .attr("height", h+10);
+}
+
+setupDependencies();
+setupStructure();
+
+//initSmartReviews();
+
+
+
+
